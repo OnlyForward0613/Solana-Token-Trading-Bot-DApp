2e0523f3a960e294c8f87082643f137b07e7a54f
OnlyForward0613 <daveescott059@gmail.com>
2025-04-22 15:27:35 -0400

    updated backend link

--- src/lib/apollo.tsx
+++ src/lib/apollo.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://solana-token-trading-bot-synesxi-be.vercel.app/graphql',
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
